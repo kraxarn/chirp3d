05abc2cc7d3a4af669ee45170b6ff7c6c8e3bac0
kraxarn <kraxie@protonmail.com>
2025-12-23 17:06:53 +0100

    Initial camera

---
+++ include/camera.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "math.h"
+
+typedef struct camera_t
+{
+	vector3f_t position;
+	vector3f_t target;
+	vector3f_t up;
+	float fov_y;
+	float near_plane;
+	float far_plane;
+} camera_t;
