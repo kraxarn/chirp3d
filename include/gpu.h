8b53b8d7ce9b87d74c6d28398e9d5bf2a78cde76
kraxarn <kraxie@protonmail.com>
2025-12-23 18:07:06 +0100

    Add pipeline abstraction

--- include/gpu.h
+++ include/gpu.h
@@ -4,3 +4,6 @@
 #include <SDL3/SDL_video.h>
 
 SDL_GPUDevice *create_device(SDL_Window *window);
+
+SDL_GPUGraphicsPipeline *create_pipeline(SDL_GPUDevice *device, SDL_Window *window,
+	SDL_GPUShader *vertex_shader, SDL_GPUShader *fragment_shader);
--- src/gpu.c
+++ src/gpu.c
@@ -1,4 +1,5 @@
 #include "gpu.h"
+#include "mesh.h"
 
 #include <SDL3/SDL_gpu.h>
 #include <SDL3/SDL_video.h>
@@ -30,4 +31,57 @@ SDL_GPUDevice *create_device(SDL_Window *window)
 	}
 
 	return device;
+}
+
+SDL_GPUGraphicsPipeline *create_pipeline(SDL_GPUDevice *device, SDL_Window *window,
+	SDL_GPUShader *vertex_shader, SDL_GPUShader *fragment_shader)
+{
+	const SDL_GPUGraphicsPipelineCreateInfo create_info = {
+		.target_info = (SDL_GPUGraphicsPipelineTargetInfo){
+			.num_color_targets = 1,
+			.color_target_descriptions = (SDL_GPUColorTargetDescription[]){
+				(SDL_GPUColorTargetDescription){
+					.format = SDL_GetGPUSwapchainTextureFormat(device, window),
+				}
+			},
+			.has_depth_stencil_target = true,                        // ?
+			.depth_stencil_format = SDL_GPU_TEXTUREFORMAT_D16_UNORM, // ?
+		},
+		.vertex_input_state = (SDL_GPUVertexInputState){
+			.num_vertex_buffers = 1,
+			.vertex_buffer_descriptions = (SDL_GPUVertexBufferDescription[]){
+				(SDL_GPUVertexBufferDescription){
+					.slot = 0,
+					.pitch = sizeof(vertex_t),
+					.input_rate = SDL_GPU_VERTEXINPUTRATE_VERTEX,
+				},
+			},
+			.num_vertex_attributes = 2,
+			.vertex_attributes = (SDL_GPUVertexAttribute[]){
+				// Position
+				(SDL_GPUVertexAttribute){
+					.location = 0,
+					.format = SDL_GPU_VERTEXELEMENTFORMAT_FLOAT3,
+					.offset = 0,
+				},
+				// Colour
+				(SDL_GPUVertexAttribute){
+					.location = 1,
+					.format = SDL_GPU_VERTEXELEMENTFORMAT_FLOAT4,
+					.offset = sizeof(float) * 3,
+				},
+			},
+		},
+		.primitive_type = SDL_GPU_PRIMITIVETYPE_TRIANGLELIST,
+		.vertex_shader = vertex_shader,
+		.fragment_shader = fragment_shader,
+		.depth_stencil_state = (SDL_GPUDepthStencilState){
+			// ?
+			.enable_depth_test = true,
+			.enable_depth_write = true,
+			.compare_op = SDL_GPU_COMPAREOP_LESS,
+		},
+	};
+
+	return SDL_CreateGPUGraphicsPipeline(device, &create_info);
 }
