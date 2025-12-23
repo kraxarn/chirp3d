289de19d7ce592d80474ce7fa16d7665ed3d4a74
kraxarn <kraxie@protonmail.com>
2025-12-23 17:38:27 +0100

    Remove old stuff from math

--- include/math.h
+++ include/math.h
@@ -1,45 +1,4 @@
 #pragma once
 
-typedef struct vector2f_t
-{
-	float x;
-	float y;
-} vector2f_t;
-
-typedef struct vector3f_t
-{
-	float x;
-	float y;
-	float z;
-} vector3f_t;
-
-typedef struct vector4f_t
-{
-	float x;
-	float y;
-	float z;
-	float w;
-} vector4f_t;
-
-typedef struct matrix4x4_t
-{
-	float m[16];
-} matrix4x4_t;
-
 [[nodiscard]]
-float clamp(float value, float min, float max);
-
-[[nodiscard]]
-float lerp(float start, float end, float amount);
-
-[[nodiscard]]
-float normalize(float value, float start, float end);
-
-[[nodiscard]]
-float remap(float value, float in_start, float in_end, float out_start, float out_end);
-
-[[nodiscard]]
-float wrap(float value, float min, float max);
-
-[[nodiscard]]
-bool eqf(float value1, float value2);
+float deg2rad(float degrees);
--- src/math.c
+++ src/math.c
@@ -2,32 +2,7 @@
 
 #include <SDL3/SDL_stdinc.h>
 
-float clamp(const float value, const float min, const float max)
+float deg2rad(const float degrees)
 {
-	return SDL_min(max, SDL_max(min, value));
-}
-
-float lerp(const float start, const float end, const float amount)
-{
-	return start + (amount * (end - start));
-}
-
-float normalize(const float value, const float start, const float end)
-{
-	return (value - start) / (end - start);
-}
-
-float remap(const float value, const float in_start, const float in_end, const float out_start, const float out_end)
-{
-	return ((value - in_start) / (in_end - in_start) * (out_end - out_start)) + out_start;
-}
-
-float wrap(const float value, const float min, const float max)
-{
-	return value - ((max - min) * SDL_floorf(((value - min) / max) - min));
-}
-
-bool eqf(const float value1, const float value2)
-{
-	return SDL_fabsf(value1 - value2) <= SDL_FLT_EPSILON * SDL_max(1.F, SDL_max(SDL_fabsf(value1), SDL_fabsf(value2)));
+	return degrees * SDL_PI_F / 180.F;
 }
