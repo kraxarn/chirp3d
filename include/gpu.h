5904a4981c8111b1ee25fca379036df8eae9065e
kraxarn <kraxie@protonmail.com>
2025-12-23 18:30:11 +0100

    oops

--- include/gpu.h
+++ include/gpu.h
@@ -8,7 +8,7 @@
 SDL_GPUDevice *create_device(SDL_Window *window);
 
 SDL_GPUGraphicsPipeline *create_pipeline(SDL_GPUDevice *device, SDL_Window *window,
-	SDL_GPUBuffer *vertex_buffer, SDL_GPUBuffer *index_buffer);
+	SDL_GPUShader *vertex_shader, SDL_GPUShader *fragment_shader);
 
 bool create_mesh_buffers(SDL_GPUDevice *device, mesh_t mesh,
 	SDL_GPUBuffer **vertex_buffer, SDL_GPUBuffer **index_buffer);
