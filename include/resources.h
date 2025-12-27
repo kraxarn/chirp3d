a800c72379bb92fa3a2d20e75bf776b9180d408b
kraxarn <kraxie@protonmail.com>
2025-12-27 20:51:32 +0100

    Fix resource paths

--- include/resources.h
+++ include/resources.h
@@ -4,35 +4,35 @@
 
 const Uint8 shader_default_vert_dxil[] =
 {
-#embed "resources/shaders/dxil/default.vert.dxil"
+#embed SHADER_DEFAULT_VERT_DXIL_PATH
 };
 
 const Uint8 shader_default_frag_dxil[] =
 {
-#embed "resources/shaders/dxil/default.frag.dxil"
+#embed SHADER_DEFAULT_FRAG_DXIL_PATH
 };
 
 const Uint8 shader_default_vert_msl[] =
 {
-#embed "resources/shaders/msl/default.vert.msl"
+#embed SHADER_DEFAULT_VERT_MSL_PATH
 };
 
 const Uint8 shader_default_frag_msl[] =
 {
-#embed "resources/shaders/msl/default.frag.msl"
+#embed SHADER_DEFAULT_FRAG_MSL_PATH
 };
 
 const Uint8 shader_default_vert_spv[] =
 {
-#embed "resources/shaders/spv/default.vert.spv"
+#embed SHADER_DEFAULT_VERT_SPV_PATH
 };
 
 const Uint8 shader_default_frag_spv[] =
 {
-#embed "resources/shaders/spv/default.frag.spv"
+#embed SHADER_DEFAULT_FRAG_SPV_PATH
 };
 
 const Uint8 texture_wall_qoi[] =
 {
-#embed "resources/textures/wall.qoi"
+#embed TEXTURE_WALL_QOI_PATH
 };
---
+++ resources.cmake
@@ -0,0 +1,13 @@
+set(RESOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/resources")
+
+target_compile_definitions(${PROJECT_NAME} PRIVATE
+	# Shaders
+	SHADER_DEFAULT_VERT_DXIL_PATH="${RESOURCE_DIR}/shaders/dxil/default.vert.dxil"
+	SHADER_DEFAULT_FRAG_DXIL_PATH="${RESOURCE_DIR}/shaders/dxil/default.frag.dxil"
+	SHADER_DEFAULT_VERT_MSL_PATH="${RESOURCE_DIR}/shaders/msl/default.vert.msl"
+	SHADER_DEFAULT_FRAG_MSL_PATH="${RESOURCE_DIR}/shaders/msl/default.frag.msl"
+	SHADER_DEFAULT_VERT_SPV_PATH="${RESOURCE_DIR}/shaders/spv/default.vert.spv"
+	SHADER_DEFAULT_FRAG_SPV_PATH="${RESOURCE_DIR}/shaders/spv/default.frag.spv"
+	# Textures
+	TEXTURE_WALL_QOI_PATH="${RESOURCE_DIR}/textures/wall.qoi"
+)
