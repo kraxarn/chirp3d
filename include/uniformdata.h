18f0e4b38308f855307e7b54487675d8faaeddcc
kraxarn <kraxie@protonmail.com>
2025-12-27 14:17:33 +0100

    Remove unused uniform data

--- include/uniformdata.h
+++ include/uniformdata.h
@@ -1,15 +1,8 @@
 #pragma once
 
 #include "matrix.h"
-#include "pointlight.h"
-#include "vector.h"
-
-#include <SDL3/SDL_pixels.h>
 
 typedef struct vertex_uniform_data_t
 {
 	matrix4x4_t mvp;
-	SDL_FColor color;
-	vector4f_t camera_position;
-	point_light_t lights[2];
 } vertex_uniform_data_t;
