18efd7b17d12fe3388f986044a14ca8f3b78e648
kraxarn <kraxie@protonmail.com>
2025-12-27 14:18:11 +0100

    Add texture coordinates for vertex

--- include/meshinfo.h
+++ include/meshinfo.h
@@ -2,7 +2,6 @@
 
 #include "vector.h"
 
-#include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_stdinc.h>
 
 typedef Uint16 mesh_index_t;
@@ -10,8 +9,7 @@ typedef Uint16 mesh_index_t;
 typedef struct vertex_t
 {
 	vector3f_t position;
-	vector3f_t normal;
-	SDL_FColor color;
+	vector2f_t tex_coord;
 } vertex_t;
 
 typedef struct mesh_info_t
