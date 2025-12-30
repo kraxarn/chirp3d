f4eeae8da22c574e86227540826029a5ef9151e7
kraxarn <kraxie@protonmail.com>
2025-12-30 15:01:59 +0100

    Fix missing property

--- include/appstate.h
+++ include/appstate.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "camera.h"
+#include "font.h"
 #include "mesh.h"
 
 #include <SDL3/SDL_gpu.h>
-#include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
 
 typedef struct app_state_t
@@ -12,6 +12,7 @@ typedef struct app_state_t
 	SDL_Window *window;
 	SDL_GPUDevice *device;
 	SDL_GPUGraphicsPipeline *pipeline;
+	font_t *font;
 	mesh_t *mesh;
 	camera_t camera;
 	Uint64 last_update;
