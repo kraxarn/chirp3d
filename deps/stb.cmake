7e439d57d2110ac27df694711326468491dc0cfb
kraxarn <kraxie@protonmail.com>
2026-01-06 14:29:21 +0100

    Remove unused library

--- deps/stb.cmake
+++ deps/stb.cmake
@@ -11,14 +11,3 @@ FetchContent_MakeAvailable(stb)
 target_include_directories(${PROJECT_NAME} PRIVATE
 	"${stb_SOURCE_DIR}"
 )
-
-function(add_stb_library NAME)
-	string(TOUPPER "${NAME}" NAME_UPPER)
-	add_library(${NAME} SHARED)
-	set_target_properties(${NAME} PROPERTIES LINKER_LANGUAGE C)
-	target_compile_definitions(${NAME} PRIVATE "${NAME_UPPER}_IMPLEMENTATION")
-	target_sources(${NAME} PRIVATE "${stb_SOURCE_DIR}/${NAME}.h")
-	target_link_libraries(${PROJECT_NAME} PRIVATE "${NAME}")
-endfunction()
-
-add_stb_library(stb_ds)
