df2d8984f734d54b87cf91e3866c83b72d1a6002
kraxarn <kraxie@protonmail.com>
2026-01-08 19:25:09 +0100

    Some physics shape stuff for later

--- include/physics.h
+++ include/physics.h
@@ -1,8 +1,34 @@
 #pragma once
 
+#include "vector.h"
+
+#include <SDL3/SDL_stdinc.h>
+
 typedef struct physics_engine_t physics_engine_t;
+
+typedef enum object_layer_t: Uint32
+{
+	OBJ_LAYER_NON_MOVING = 0,
+	OBJ_LAYER_MOVING     = 1,
+	OBJ_LAYER_COUNT      = 2,
+} object_layer_t;
+
+typedef enum physics_motion_type_t
+{
+	MOTION_TYPE_STATIC    = 0,
+	MOTION_TYPE_KINEMATIC = 1,
+	MOTION_TYPE_DYNAMIC   = 2,
+} physics_motion_type_t;
 
 [[nodiscard]]
 physics_engine_t *physics_engine_create();
 
 void physics_engine_destroy(physics_engine_t *engine);
+
+void physics_engine_optimize(const physics_engine_t *engine);
+
+void physics_engine_add_box(physics_engine_t *engine, physics_motion_type_t motion_type,
+	object_layer_t layer, vector3f_t position, vector3f_t half_extents);
+
+void physics_engine_add_sphere(physics_engine_t *engine, physics_motion_type_t motion_type,
+	object_layer_t layer, vector3f_t position, float radius);
--- src/physics.c
+++ src/physics.c
@@ -1,5 +1,6 @@
 #include "physics.h"
 #include "logcategory.h"
+#include "vector.h"
 
 #include "joltc.h"
 
@@ -7,20 +8,7 @@
 #include <SDL3/SDL_error.h>
 #include <SDL3/SDL_log.h>
 #include <SDL3/SDL_stdinc.h>
-
-typedef struct physics_engine_t
-{
-	JPH_JobSystem *job_system;
-	JPH_PhysicsSystem *physics_system;
-	JPH_BodyInterface *body_interface;
-} physics_engine_t;
-
-typedef enum object_layers_t: JPH_ObjectLayer
-{
-	OBJ_LAYER_NON_MOVING = 0,
-	OBJ_LAYER_MOVING     = 1,
-	OBJ_LAYER_COUNT      = 2,
-} object_layers_t;
+#include <SDL3/SDL_timer.h>
 
 typedef enum broad_phase_layers_t: JPH_BroadPhaseLayer
 {
@@ -34,6 +22,16 @@ static constexpr Uint32 max_bodies = SDL_MAX_UINT16;
 static constexpr Uint32 num_body_mutexes = 0;
 static constexpr Uint32 max_body_pairs = SDL_MAX_UINT16;
 static constexpr Uint32 max_contact_constraints = SDL_MAX_UINT16;
+
+typedef struct physics_engine_t
+{
+	JPH_JobSystem *job_system;
+	JPH_PhysicsSystem *physics_system;
+	JPH_BodyInterface *body_interface;
+
+	JPH_BodyID bodies[max_bodies]; // TODO: Is this a bit excessive maybe?
+	size_t num_bodies;
+} physics_engine_t;
 
 static void on_trace(const char *message)
 {
@@ -72,6 +70,7 @@ physics_engine_t *physics_engine_create()
 		return nullptr;
 	}
 
+	engine->num_bodies = 0;
 	engine->job_system = JPH_JobSystemThreadPool_Create(nullptr);
 
 	JPH_ObjectLayerPairFilter *obj_filter = JPH_ObjectLayerPairFilterTable_Create(OBJ_LAYER_COUNT);
@@ -126,4 +125,66 @@ void physics_engine_destroy(physics_engine_t *engine)
 	JPH_Shutdown();
 
 	SDL_free(engine);
+}
+
+void physics_engine_optimize(const physics_engine_t *engine)
+{
+	const Uint64 start = SDL_GetTicks();
+
+	JPH_PhysicsSystem_OptimizeBroadPhase(engine->physics_system);
+
+	const Uint64 end = SDL_GetTicks();
+	SDL_LogDebug(LOG_CATEGORY_PHYSICS, "Optimised broad phase in %llu ms", end - start);
+}
+
+static JPH_Vec3 jph_vec3(const vector3f_t vec)
+{
+	// TODO: They should be exactly the same, so we can probably just cast directly
+	return (JPH_Vec3){
+		.x = vec.x,
+		.y = vec.y,
+		.z = vec.z,
+	};
+}
+
+static JPH_MotionType jph_motion_type(const physics_motion_type_t motion_type)
+{
+	// Should always match, but keep in function just in case
+	return (JPH_MotionType) motion_type;
+}
+
+static void add_body(physics_engine_t *engine,
+	const JPH_BodyCreationSettings *settings, const JPH_Activation activation)
+{
+	const JPH_BodyID body = JPH_BodyInterface_CreateAndAddBody(engine->body_interface, settings, activation);
+	engine->bodies[engine->num_bodies++] = body;
+}
+
+void physics_engine_add_box(physics_engine_t *engine, const physics_motion_type_t motion_type,
+	const object_layer_t layer, const vector3f_t position, const vector3f_t half_extents)
+{
+	const JPH_Vec3 jph_half_extents = jph_vec3(half_extents);
+	JPH_BoxShape *shape = JPH_BoxShape_Create(&jph_half_extents, JPH_DEFAULT_CONVEX_RADIUS);
+
+	const JPH_Vec3 jph_position = jph_vec3(position);
+	JPH_BodyCreationSettings *settings = JPH_BodyCreationSettings_Create3(
+		(JPH_Shape *) shape, &jph_position, nullptr,
+		jph_motion_type(motion_type), layer
+	);
+	add_body(engine, settings, JPH_Activation_DontActivate);
+	JPH_BodyCreationSettings_Destroy(settings);
+}
+
+void physics_engine_add_sphere(physics_engine_t *engine, const physics_motion_type_t motion_type,
+	const object_layer_t layer, const vector3f_t position, const float radius)
+{
+	JPH_SphereShape *shape = JPH_SphereShape_Create(radius);
+
+	const JPH_Vec3 jph_position = jph_vec3(position);
+	JPH_BodyCreationSettings *settings = JPH_BodyCreationSettings_Create3(
+		(JPH_Shape *) shape, &jph_position, nullptr,
+		jph_motion_type(motion_type), layer
+	);
+	add_body(engine, settings, JPH_Activation_Activate);
+	JPH_BodyCreationSettings_Destroy(settings);
 }
