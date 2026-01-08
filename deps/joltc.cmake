94ccc3fb38b76aca4bb865f61274f27f43b8d56c
kraxarn <kraxie@protonmail.com>
2026-01-08 15:24:54 +0100

    Add joltc

---
+++ deps/joltc.cmake
@@ -0,0 +1,11 @@
+include(FetchContent)
+
+FetchContent_Declare(joltc
+	GIT_REPOSITORY https://github.com/amerkoleci/joltc.git
+	GIT_TAG cedc753d4cc9a1be565298b293dd09af44240074
+)
+
+message(STATUS "Downloading joltc")
+FetchContent_MakeAvailable(joltc)
+
+target_link_libraries(${PROJECT_NAME} PRIVATE joltc)
