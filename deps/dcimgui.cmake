33806c3eef0525052e10ccfdee2ccae1165218ed
kraxarn <kraxie@protonmail.com>
2026-01-14 15:10:24 +0100

    Try using dear_bindings instead and generate bindings

---
+++ deps/dcimgui.cmake
@@ -0,0 +1,55 @@
+include(FetchContent)
+
+FetchContent_Declare(dcimgui
+	GIT_REPOSITORY https://github.com/dearimgui/dear_bindings.git
+	GIT_TAG DearBindings_v0.17_ImGui_v1.92.5
+)
+
+message(STATUS "Downloading dcimgui")
+FetchContent_MakeAvailable(dcimgui)
+
+add_library(dcimgui STATIC)
+
+set(VENV "${dcimgui_SOURCE_DIR}/venv")
+
+add_custom_command(OUTPUT
+	"${dcimgui_SOURCE_DIR}/generated/dcimgui.cpp"
+	"${dcimgui_SOURCE_DIR}/generated/dcimgui.h"
+	"${dcimgui_SOURCE_DIR}/generated/backends/dcimgui_impl_sdlgpu3.cpp"
+	"${dcimgui_SOURCE_DIR}/generated/backends/dcimgui_impl_sdlgpu3.h"
+
+	COMMAND python3 -m venv "${VENV}"
+	COMMAND "${VENV}/bin/pip3" install -r "${dcimgui_SOURCE_DIR}/requirements.txt"
+
+	COMMAND "${VENV}/bin/python3" "${dcimgui_SOURCE_DIR}/dear_bindings.py"
+	--output "${dcimgui_SOURCE_DIR}/generated/dcimgui"
+	"${imgui_SOURCE_DIR}/imgui.h"
+
+	COMMAND "${VENV}/bin/python3" "${dcimgui_SOURCE_DIR}/dear_bindings.py"
+	--backend
+	--include "${imgui_SOURCE_DIR}/imgui.h"
+	--imconfig-path "${imgui_SOURCE_DIR}/imconfig.h"
+	--output "${dcimgui_SOURCE_DIR}/generated/backends/dcimgui_impl_sdlgpu3"
+	"${imgui_SOURCE_DIR}/backends/imgui_impl_sdlgpu3.h"
+
+	VERBATIM
+)
+
+file(REMOVE_RECURSE "${VENV}")
+
+target_link_libraries(dcimgui PRIVATE imgui SDL3::SDL3)
+
+target_include_directories(dcimgui PRIVATE
+	"${imgui_SOURCE_DIR}/backends"
+)
+
+target_sources(dcimgui PRIVATE
+	"${dcimgui_SOURCE_DIR}/generated/dcimgui.cpp"
+	"${dcimgui_SOURCE_DIR}/generated/backends/dcimgui_impl_sdlgpu3.cpp"
+)
+
+target_include_directories(dcimgui PUBLIC
+	"${dcimgui_SOURCE_DIR}/generated"
+)
+
+target_link_libraries(${PROJECT_NAME} PRIVATE dcimgui)
