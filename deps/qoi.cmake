c73adbb79cc0e22c6673424caced95ad37bd3bf1
kraxarn <kraxie@protonmail.com>
2026-03-02 15:28:11 +0100

    Update qoi

--- deps/qoi.cmake
+++ deps/qoi.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(qoi
 	GIT_REPOSITORY https://github.com/phoboslab/qoi.git
-	GIT_TAG 44b233a95eda82fbd2e39a269199b73af0f4c4c3
+	GIT_TAG 6fff9b70dd79b12f808b0acc5cb44fde9998725e
 )
 
 message(STATUS "Downloading qoi")
