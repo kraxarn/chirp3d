448588a98ef2406f38d0f819958af9ab45da5d7c
kraxarn <git@kraxarn.com>
2026-05-20 19:50:04 +0200

    Remove model projection to make instancing easier later

--- include/model.h
+++ include/model.h
@@ -24,14 +24,14 @@ model_t *model_create(SDL_GPUDevice *device, SDL_IOStream *stream, bool close_io
 
 void model_destroy(model_t *model);
 
-void model_draw(model_t *model, SDL_GPURenderPass *render_pass,
+void model_draw(const model_t *model, SDL_GPURenderPass *render_pass,
 	SDL_GPUCommandBuffer *command_buffer, matrix4x4_t projection);
 
-void model_set_rotation(model_t *model, vector3f_t rotation);
+void model_set_rotation(const model_t *model, vector3f_t rotation);
 
 [[nodiscard]]
 vector3f_t model_node_position(const model_t *model, const char *node);
 
-void model_set_position(model_t *model, vector3f_t position);
+void model_set_position(const model_t *model, vector3f_t position);
 
-void model_set_scale(model_t *model, vector3f_t scale);
+void model_set_scale(const model_t *model, vector3f_t scale);
