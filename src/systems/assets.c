86794b44251cd87951d168ddff857357bba04fee
kraxarn <git@kraxarn.com>
2026-06-11 14:49:03 +0200

    Remove macros

--- src/systems/assets.c
+++ src/systems/assets.c
@@ -18,19 +18,27 @@ static void on_file_opened([[maybe_unused]] void *userdata,
 	assets_t assets;
 	if (assets_create(filelist[0], &assets))
 	{
-		ECS_COMPONENT(ecs_world(), Assets);
-		ecs_singleton_set(ecs_world(), Assets, {
-			.stream = assets.stream,
-			.window_config = assets.window_config,
-			.desc = assets.desc,
-			}
-		);
+		const ecs_entity_t entity = ecs_lookup(ecs_world(), "Assets");
+		ecs_set_id(ecs_world(), entity, entity,
+			sizeof(Assets), &assets);
 	}
 }
 
 void system_register_assets()
 {
-	ECS_COMPONENT(ecs_world(), Assets);
+	const ecs_entity_desc_t entity_desc = {
+		.use_low_id = true,
+		.name = "Assets",
+		.symbol = "Assets",
+	};
+	const ecs_component_desc_t component_desc = {
+		.entity = ecs_entity_init(ecs_world(), &entity_desc),
+		.type = (ecs_type_info_t){
+			.size = ECS_SIZEOF(Assets),
+			.alignment = ECS_ALIGNOF(Assets),
+		},
+	};
+	ecs_component_init(ecs_world(), &component_desc);
 
 	const char *base_path = SDL_GetBasePath();
 	const size_t path_len = SDL_strlen(base_path) + SDL_arraysize("assets.nest") + 1;
@@ -42,12 +50,11 @@ void system_register_assets()
 	if (assets_create(path, &assets))
 	{
 		SDL_free(path);
-		ecs_singleton_set(ecs_world(), Assets, {
-			.stream = assets.stream,
-			.window_config = assets.window_config,
-			.desc = assets.desc,
-			}
-		);
+
+		const ecs_entity_t entity = ecs_lookup(ecs_world(), "Assets");
+		ecs_set_id(ecs_world(), entity, entity,
+			sizeof(Assets), &assets);
+
 		return;
 	}
 	SDL_free(path);
